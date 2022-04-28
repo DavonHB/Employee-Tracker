6e99067f8f0a5e2c10746ad190767320d8cdd9ae
Nosferatu <davonb113@gmail.com>
2022-04-28 18:06:03 -0500

    added index polsihing to run file

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //require dependencies 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const util = require('util');
-const { connect } = require('http2');
 
 //create connection to workbench 
 let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'Desdavon1713!',
     database: 'employee_DB'
+    
 });
 
 connection.query = util.promisify(connection.query);
